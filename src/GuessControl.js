80e93a6c32e3930b8af19043c0a87d11893b8b32
Masooma <masuma.jafari@gmail.com>
2023-06-05 10:14:45 -0700

    Convert GuessControl and NumberGussingGame class to functioncomponent

--- src/GuessControl.js
+++ src/GuessControl.js
@@ -1,7 +1,28 @@
 import React, { Component } from "react";
 import Button from "./Button";
+import { useState } from "react";
 
-class GuessControl extends Component {
+const GuessControl = ({ onGuess }) => {
+  const [currentGuess, setCurrentGuess] = React.useState("");
+
+  function handleInputChange(event) {
+    setCurrentGuess(event.target.value);
+  }
+
+  function onSubmitGuess() {
+    onGuess(Number(currentGuess));
+    setCurrentGuess("");
+  }
+
+  return (
+    <div>
+      <input type="number" value={currentGuess} onChange={handleInputChange} />
+      <Button onClick={onSubmitGuess}>Submit Guess</Button>
+    </div>
+  );
+};
+
+class GuessControlOld extends Component {
   constructor(props) {
     super(props);
 
