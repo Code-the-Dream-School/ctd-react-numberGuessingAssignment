c6a71ca578211c960aade6bf00ce6a1b1be0cc56
pstnv <pstnv@mail.ru>
2023-11-06 00:20:55 +0500

    change NumberGuessingGame  component from class to function

--- src/NumberGuessingGame.js
+++ src/NumberGuessingGame.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import GuessControl from "./GuessControl";
 import GuessMessage from "./GuessMessage";
 import GameOver from "./GameOver";
@@ -8,75 +8,51 @@ import GameOver from "./GameOver";
  * Returns a random integer number from 1-100 inclusive
  */
 function getRandomNumber() {
-    return Math.floor(Math.random() * 100) + 1;
+    const ranNum = Math.floor(Math.random() * 100) + 1;
+    console.log(ranNum);
+    return ranNum;
 }
 
 const MAX_ATTEMPTS = 5;
+// use this variable outside to avoid its rendering everytime
+let numberToGuess = getRandomNumber();
 
-class NumberGuessingGame extends Component {
-    constructor(props) {
-        super(props);
+function NumberGuessingGame() {
+    const [numberOfGuesses, setNumberOfGuesses] = useState(0);
+    const [latestGuess, setLatestGuess] = useState(null);
 
-        this.state = {
-            numberToGuess: getRandomNumber(),
-            numberOfGuesses: 0,
-            latestGuess: null,
-        };
+    const handleGuess = (guess) => {
+        setLatestGuess(guess);
+        setNumberOfGuesses(numberOfGuesses + 1);
+    };
+    const handleReset = () => {
+        numberToGuess = getRandomNumber();
+        setLatestGuess(null);
+        setNumberOfGuesses(0);
+    };
 
-        /**
-         * These lines are required to make the methods/functions declared on this
-         *  class have the correct `this` object when they run.
-         */
-        this.handleGuess = this.handleGuess.bind(this);
-        this.handleReset = this.handleReset.bind(this);
-    }
+    const isCorrectGuess = latestGuess === numberToGuess;
+    const isGameOver = isCorrectGuess || numberOfGuesses === MAX_ATTEMPTS;
 
-    handleGuess(guess) {
-        this.setState({
-            latestGuess: guess,
-            numberOfGuesses: this.state.numberOfGuesses + 1,
-        });
-    }
-
-    handleReset() {
-        this.setState({
-            numberToGuess: getRandomNumber(),
-            numberOfGuesses: 0,
-            latestGuess: null,
-        });
-    }
-
-    render() {
-        const isCorrectGuess =
-            this.state.latestGuess === this.state.numberToGuess;
-
-        const isGameOver =
-            isCorrectGuess || this.state.numberOfGuesses === MAX_ATTEMPTS;
-
-        return (
-            <div>
-                <h2>I'm thinking of a number from 1 to 100.</h2>
-                <h2>
-                    Can you guess the number I am thinking of in {MAX_ATTEMPTS}{" "}
-                    tries?
-                </h2>
-                <GuessControl onGuess={this.handleGuess} />
-                {isGameOver && (
-                    <GameOver
-                        hasWon={isCorrectGuess}
-                        onReset={this.handleReset}
-                    />
-                )}
-                {!isGameOver && (
-                    <GuessMessage
-                        guess={this.state.latestGuess}
-                        numberToGuess={this.state.numberToGuess}
-                        numberOfGuesses={this.state.numberOfGuesses}
-                    />
-                )}
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h2>I'm thinking of a number from 1 to 100.</h2>
+            <h2>
+                Can you guess the number I am thinking of in {MAX_ATTEMPTS}{" "}
+                tries?
+            </h2>
+            <GuessControl onGuess={handleGuess} />
+            {isGameOver && (
+                <GameOver hasWon={isCorrectGuess} onReset={handleReset} />
+            )}
+            {!isGameOver && (
+                <GuessMessage
+                    guess={latestGuess}
+                    numberToGuess={numberToGuess}
+                    numberOfGuesses={numberOfGuesses}
+                />
+            )}
+        </div>
+    );
 }
-
 export default NumberGuessingGame;
