eb7c98844c8293fef156378df8c0ebb8aed679f8
Alevtina-Babinskaya <letters4alka@gmail.com>
2024-01-08 00:34:38 -0800

    minor change

--- src/NumberGuessingGame.js
+++ src/NumberGuessingGame.js
@@ -13,67 +13,6 @@ function getRandomNumber() {
 
 const MAX_ATTEMPTS = 5;
 
-// class NumberGuessingGameOld extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       numberToGuess: getRandomNumber(),
-//       numberOfGuesses: 0,
-//       latestGuess: null,
-//     };
-
-//     /**
-//      * These lines are required to make the methods/functions declared on this
-//      *  class have the correct `this` object when they run.
-//      */
-//     this.handleGuess = this.handleGuess.bind(this);
-//     this.handleReset = this.handleReset.bind(this);
-//   }
-
-//   handleGuess(guess) {
-//     this.setState({
-//       latestGuess: guess,
-//       numberOfGuesses: this.state.numberOfGuesses + 1,
-//     });
-//   }
-
-//   handleReset() {
-//     this.setState({
-//       numberToGuess: getRandomNumber(),
-//       numberOfGuesses: 0,
-//       latestGuess: null,
-//     });
-//   }
-
-//   render() {
-//     const isCorrectGuess = this.state.latestGuess === this.state.numberToGuess;
-
-//     const isGameOver =
-//       isCorrectGuess || this.state.numberOfGuesses === MAX_ATTEMPTS;
-
-//     return (
-//       <div>
-//         <h2>I'm thinking of a number from 1 to 100.</h2>
-//         <h2>
-//           Can you guess the number I am thinking of in {MAX_ATTEMPTS} tries?
-//         </h2>
-//         <GuessControl onGuess={this.handleGuess} />
-//         {isGameOver && (
-//           <GameOver hasWon={isCorrectGuess} onReset={this.handleReset} />
-//         )}
-//         {!isGameOver && (
-//           <GuessMessage
-//             guess={this.state.latestGuess}
-//             numberToGuess={this.state.numberToGuess}
-//             numberOfGuesses={this.state.numberOfGuesses}
-//           />
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
 function NumberGuessingGame() {
   const [numberToGuess, setNumberToGuess] = useState(getRandomNumber());
   const [numberOfGuesses, setNumberOfGuesses] = useState(0);
